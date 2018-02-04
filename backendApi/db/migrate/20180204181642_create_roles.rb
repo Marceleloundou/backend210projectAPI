6fef58fba17684ab9b03e556930606c55fedb28a
Marcel Eloundou <marcel.eloundou-essimi.1@etsmtl.net>
2018-02-04 15:18:39 -0500

    creation des cles pour role et user

--- backendApi/db/migrate/20180204181642_create_roles.rb
+++ backendApi/db/migrate/20180204181642_create_roles.rb
@@ -3,7 +3,7 @@ class CreateRoles < ActiveRecord::Migration[5.1]
     create_table :roles do |t|
       t.string :nom_du_role
       t.string :acces
-      t.refences :User foreign_key: true
+      t.refences :User, foreign_key do |t|
 
       t.timestamps
     end
